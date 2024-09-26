822459cab9cb70099f48c687d7d9c00c41d4f2fd
deswalcodes <21cs2014@rgipt.ac.in>
2024-09-26 19:23:18 +0530

    initialised routers

---
+++ routes/course.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Router = express.Router;
+const courseRouter = Router();
+
+courseRouter.get('/preview',function(req,res){
+    
+})
+courseRouter.get('/purchases',function(req,res){
+    
+})
+
+module.exports = {
+    courseRouter : courseRouter
+}
---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Router = express.Router;
+const userRouter = Router();
+
+
+userRouter.post('/signup',function(req,res){
+
+
+})
+
+userRouter.post('/signin',function(req,res){
+
+})
+
+
+userRouter.get('/purchases',function(req,res){
+    
+});
+
+module.exports = {
+    userRouter : userRouter
+}
